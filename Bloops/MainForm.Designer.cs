360ca898bd8fdbc6a31eb6046f94be956ae67a4a
Dan Carter <none@none>
2016-08-11 21:26:05 +0100

    Add intro form

---
+++ Bloops/MainForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Bloops
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleBloopsButton = new System.Windows.Forms.Button();
+            this.neuralGraphButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // simpleBloopsButton
+            // 
+            this.simpleBloopsButton.Location = new System.Drawing.Point(12, 12);
+            this.simpleBloopsButton.Name = "simpleBloopsButton";
+            this.simpleBloopsButton.Size = new System.Drawing.Size(258, 39);
+            this.simpleBloopsButton.TabIndex = 0;
+            this.simpleBloopsButton.Text = "Simple bloops";
+            this.simpleBloopsButton.UseVisualStyleBackColor = true;
+            this.simpleBloopsButton.Click += new System.EventHandler(this.SimpleBloopsButton_Click);
+            // 
+            // neuralGraphButton
+            // 
+            this.neuralGraphButton.Location = new System.Drawing.Point(12, 57);
+            this.neuralGraphButton.Name = "neuralGraphButton";
+            this.neuralGraphButton.Size = new System.Drawing.Size(258, 39);
+            this.neuralGraphButton.TabIndex = 1;
+            this.neuralGraphButton.Text = "Neural graph";
+            this.neuralGraphButton.UseVisualStyleBackColor = true;
+            this.neuralGraphButton.Click += new System.EventHandler(this.NeuralGraphButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 109);
+            this.Controls.Add(this.neuralGraphButton);
+            this.Controls.Add(this.simpleBloopsButton);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button simpleBloopsButton;
+        private System.Windows.Forms.Button neuralGraphButton;
+    }
+}
---
+++ Bloops/MainForm.cs
@@ -0,0 +1,29 @@
+﻿namespace Bloops
+{
+    using System;
+    using System.Windows.Forms;
+
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            this.InitializeComponent();
+        }
+
+        private void SimpleBloopsButton_Click(object sender, EventArgs e)
+        {
+            using (BloopsForm form = new BloopsForm())
+            {
+                form.ShowDialog(this);
+            }
+        }
+
+        private void NeuralGraphButton_Click(object sender, EventArgs e)
+        {
+            using (GraphForm form = new GraphForm())
+            {
+                form.ShowDialog(this);
+            }
+        }
+    }
+}
