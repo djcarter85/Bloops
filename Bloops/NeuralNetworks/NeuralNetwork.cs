bdccf0d7f590e80dd4c759cea7c862cdfe5d30e0
Dan Carter <none@none>
2016-08-14 14:15:55 +0100

    Add neural network

---
+++ Bloops/NeuralNetworks/NeuralNetwork.cs
@@ -0,0 +1,31 @@
+﻿namespace Bloops
+{
+    using System.Collections.Generic;
+
+    class NeuralNetwork
+    {
+        private IEnumerable<NeuronLayer> layers;
+
+        public NeuralNetwork(IEnumerable<NeuronLayer> layers)
+        {
+            this.layers = layers;
+        }
+
+        public IEnumerable<NeuronLayer> Layers
+        {
+            get { return this.layers; }
+        }
+
+        public double[] Outputs(double[] inputs)
+        {
+            double[] copy = (double[])inputs.Clone();
+
+            foreach (NeuronLayer layer in this.layers)
+            {
+                copy = layer.Outputs(copy);
+            }
+
+            return copy;
+        }
+    }
+}
