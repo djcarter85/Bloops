ae228a0ccf8af20740b556f6dc33c5e5a1425091
Dan Carter <none@none>
2016-08-14 14:15:49 +0100

    Add neuron layer

---
+++ Bloops/NeuralNetworks/NeuronLayer.cs
@@ -0,0 +1,25 @@
+﻿namespace Bloops
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class NeuronLayer
+    {
+        private IEnumerable<Neuron> neurons;
+
+        public NeuronLayer(IEnumerable<Neuron> neurons)
+        {
+            this.neurons = neurons;
+        }
+
+        public IEnumerable<Neuron> Neurons
+        {
+            get { return this.neurons; }
+        }
+
+        public double[] Outputs(double[] inputs)
+        {
+            return this.neurons.Select(n => n.FeedForward(inputs)).ToArray();
+        }
+    }
+}
