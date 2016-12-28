4e4b5fb2a772ff9111df95e36dd2b5eeca4e64a7
Dan Carter <none@none>
2016-12-28 18:41:18 +0000

    Reorder methods in bloops form

--- Bloops/Bloops/BloopsForm.cs
+++ Bloops/Bloops/BloopsForm.cs
@@ -18,76 +18,6 @@
             this.map.Height = World.Height * 2;
         }
 
-        private static void DrawBloop(Bloop bloop, Graphics graphics)
-        {
-            int colour = Math.Max(255 - bloop.Health * 2, 0);
-
-            Rectangle rect = new Rectangle(
-                Convert.ToInt32(bloop.Location.X - bloop.Radius) + World.Width,
-                Convert.ToInt32(bloop.Location.Y - bloop.Radius) + World.Height,
-                Convert.ToInt32(2 * bloop.Radius),
-                Convert.ToInt32(2 * bloop.Radius));
-
-            using (SolidBrush brush = new SolidBrush(Color.FromArgb(100, colour, colour, colour)))
-            {
-                graphics.FillEllipse(brush, rect);
-            }
-
-            using (SolidBrush brush = new SolidBrush(Color.Black))
-            {
-                graphics.DrawString(bloop.Health.ToString(), new Font("Verdana", 10), brush, rect.Location);
-            }
-        }
-
-        private static void DrawFood(Vector food, Graphics graphics)
-        {
-            using (Pen pen = new Pen(Brushes.Red))
-            {
-                graphics.DrawRectangle(
-                    pen,
-                    new Rectangle(
-                        Convert.ToInt32(food.X - 1) + World.Width,
-                        Convert.ToInt32(food.Y - 1) + World.Height,
-                        3,
-                        3));
-            }
-        }
-
-        private void TimeStep()
-        {
-            this.world.Update();
-            this.map.Invalidate();
-
-            if (!this.world.Bloops.Any())
-            {
-                this.timer.Stop();
-                MessageBox.Show("All dead!");
-            }
-            else
-            {
-                this.statusLabel.Text = string.Format(
-                    "Bloops: {0}\r\nMax health: {1}",
-                    this.world.Bloops.Count(),
-                    this.world.Bloops.Max(b => b.Health));
-            }
-        }
-
-        private void Map_Paint(object sender, PaintEventArgs e)
-        {
-            if (this.world != null)
-            {
-                foreach (Bloop bloop in this.world.Bloops)
-                {
-                    DrawBloop(bloop, e.Graphics);
-                }
-
-                foreach (Vector food in this.world.Foods)
-                {
-                    DrawFood(food, e.Graphics);
-                }
-            }
-        }
-
         private void StartButton_Click(object sender, EventArgs e)
         {
             if (this.world == null)
@@ -142,6 +72,76 @@
         private void StepButton_Click(object sender, EventArgs e)
         {
             this.TimeStep();
+        }
+
+        private void TimeStep()
+        {
+            this.world.Update();
+            this.map.Invalidate();
+
+            if (!this.world.Bloops.Any())
+            {
+                this.timer.Stop();
+                MessageBox.Show("All dead!");
+            }
+            else
+            {
+                this.statusLabel.Text = string.Format(
+                    "Bloops: {0}\r\nMax health: {1}",
+                    this.world.Bloops.Count(),
+                    this.world.Bloops.Max(b => b.Health));
+            }
+        }
+
+        private void Map_Paint(object sender, PaintEventArgs e)
+        {
+            if (this.world != null)
+            {
+                foreach (Bloop bloop in this.world.Bloops)
+                {
+                    DrawBloop(bloop, e.Graphics);
+                }
+
+                foreach (Vector food in this.world.Foods)
+                {
+                    DrawFood(food, e.Graphics);
+                }
+            }
+        }
+
+        private static void DrawBloop(Bloop bloop, Graphics graphics)
+        {
+            int colour = Math.Max(255 - bloop.Health * 2, 0);
+
+            Rectangle rect = new Rectangle(
+                Convert.ToInt32(bloop.Location.X - bloop.Radius) + World.Width,
+                Convert.ToInt32(bloop.Location.Y - bloop.Radius) + World.Height,
+                Convert.ToInt32(2 * bloop.Radius),
+                Convert.ToInt32(2 * bloop.Radius));
+
+            using (SolidBrush brush = new SolidBrush(Color.FromArgb(100, colour, colour, colour)))
+            {
+                graphics.FillEllipse(brush, rect);
+            }
+
+            using (SolidBrush brush = new SolidBrush(Color.Black))
+            {
+                graphics.DrawString(bloop.Health.ToString(), new Font("Verdana", 10), brush, rect.Location);
+            }
+        }
+
+        private static void DrawFood(Vector food, Graphics graphics)
+        {
+            using (Pen pen = new Pen(Brushes.Red))
+            {
+                graphics.DrawRectangle(
+                    pen,
+                    new Rectangle(
+                        Convert.ToInt32(food.X - 1) + World.Width,
+                        Convert.ToInt32(food.Y - 1) + World.Height,
+                        3,
+                        3));
+            }
         }
     }
 }
